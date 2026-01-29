766161708e9f6e0b0a56fea154af753b053c0c63
gapeva <gapevadotpro@gmail.com>
2026-01-29 13:11:14 +0300

    Update deps.py

--- server/app/api/deps.py
+++ server/app/api/deps.py
@@ -1,1 +1,41 @@
+from typing import Generator
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from ..core import security
+from ..models import models
+from sqlalchemy import create_all, create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/little_alpha")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
